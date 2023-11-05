c6673bd136a9ea541a5e0b06163be957dfcd66e4
s4m4n <pflum.tamas@gmail.com>
2023-11-05 09:00:04 +0100

    0

--- 04-rekurzio-lnko-lkkt/rekurzio-lnko-lkkt-fak.py
+++ 04-rekurzio-lnko-lkkt/rekurzio-lnko-lkkt-fak.py
@@ -1,7 +1,39 @@
 import math
-'''
-# rekurziv legkisebb kozos tobbszoros a legnagyobb kozos oszto es a pszeudokod alapjan
+import time
+start = time.time()
+a=21712
+b=10533
+f=4
 
+if a<=b:
+    kisebb=a
+    nagyobb=b
+else:
+    kisebb=b
+    nagyobb=a
+
+print(f'szamok: {a}, {b}')
+
+'''
+#--------------------------------------
+#lnko rekurzio nelkul
+lnko=1
+for i in range(1,kisebb+1):
+    if nagyobb%i==0 and kisebb%i==0:
+        lnko=i
+print(f'{nagyobb} es {kisebb} legnagyobb kozos osztoja [rekurzio nelkul]: {lnko}')
+
+#--------------------------------------
+#lkkt rekurzio nelkul
+lkkt=a*b
+for i in range(lkkt,nagyobb-1,-1):
+    if (i%nagyobb==0 and i%kisebb==0):
+        lkkt=i
+print(f'{nagyobb} es {kisebb} legkisebb kozos tobbszorose [rekurzio nelkul]: {lkkt}')        
+
+'''
+#--------------------------------------
+#lnko rekurzioval
 def lnko(a,b):
     if (a==b):
         ln=a
@@ -9,60 +41,26 @@ def lnko(a,b):
         ln = lnko(a, b-a)
     if (a>b):
         ln = lnko(a-b, b)
-
     return ln
-
-
+print(f'{nagyobb} es {kisebb} legnagyobb kozos osztoja [rekurzioval]: {lnko(a,b)}')
+#--------------------------------------
+#lkkt rekurzioval
 def lkkt(a,b):
     lk=a*b / lnko(a,b)
     return lk
+print(f'{nagyobb} es {kisebb} legkisebb kozos tobbszorose [rekurzioval]: {"%.1d" % lkkt(a,b)}')        
 
-print(lnko(10,3))
-print("%.1d" % lkkt(10,3))
-
-# rekurziv faktorialis
-
-def fak(n):
-    print(n)
-    if n==0:
+# faktorialis rekurzioval
+def fak(f):
+    if f==0:
         return(1)
     else:
-        return(fak(n-1)*n)
+        return(fak(f-1)*f)
 
-print(fak(4))
-'''
-
-#lnko rekurzio nelkul
-a=15
-b=10
-lnko=1
-if a<=b:
-    kisebb=a
-    nagyobb=b
-else:
-    kisebb=b
-    nagyobb=a
-
-for i in range(math.floor(kisebb/2),kisebb+1):
-    if nagyobb%i==0 and kisebb%i==0:
-        lnko=i
-print(f'{nagyobb} es {kisebb} legnagyobb kozos osztoja: {lnko}')
-
-#lkkt rekurzio nelkul
-a=5
-b=9
-lkkt=a*b
-if a<=b:
-    kisebb=a
-    nagyobb=b
-else:
-    kisebb=b
-    nagyobb=a
-
-for i in range(math.floor(kisebb/2),kisebb+1):
-    if nagyobb%i==0 and kisebb%i==0:
-        lnko=i
-print(f'{nagyobb} es {kisebb} legnagyobb kozos osztoja: {lnko}')
+print(f'{f} faktorialis [rekurzioval]: {fak(f)}')
 
 
 
+
+end = time.time()
+print(f"Time taken: {(end-start)*10**3:.03f}ms")
