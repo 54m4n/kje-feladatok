470a4a6b245cf7ce4a65e9e99c291bbd784c1979
54m4n <pflum.tamas@gmail.com>
2024-02-10 12:53:08 +0100

    0

--- 05-sudoku/sudoku.py
+++ 05-sudoku/sudoku.py
@@ -1,7 +1,7 @@
 import os
 
 path=os.path.dirname(__file__)
-f=open(f'{path}{os.sep}src{os.sep}s.txt')
+f=open(f'{path}{os.sep}src{os.sep}orig.txt')
 numz=[]
 for l in f:
     numz.append(l.split())
@@ -10,5 +10,23 @@ f.close()
 
 for i in range(len(numz)):
     for j in range(9):
-        print(numz[i][j],end=' ')
-    print()
+        print(f'{numz[i][j]}',end=' ')
+        if j==2 or j==5:
+            print("|",end=' ')
+    
+    print('\n---------------------')
+
+def cenzor(c):
+    if c==1:
+        print("easy")
+    elif c==2:
+        print("medium")
+    elif c==3:
+        print("hard")
+    else:
+        print("unsolvable")
+    return
+
+cenzor(3)
+
+
