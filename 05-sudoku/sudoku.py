629bfeb9c422a2382e99f93f22a13cd9ff786362
54m4n <pflum.tamas@gmail.com>
2024-02-10 12:27:29 +0100

    0

--- 05-sudoku/sudoku.py
+++ 05-sudoku/sudoku.py
@@ -1,2 +1,14 @@
 import os
 
+path=os.path.dirname(__file__)
+f=open(f'{path}{os.sep}src{os.sep}s.txt')
+numz=[]
+for l in f:
+    numz.append(l.split())
+f.close()
+    
+
+for i in range(len(numz)):
+    for j in range(9):
+        print(numz[i][j],end=' ')
+    print()
