aded617bf5f33cdd99c334ba1929daa7a93a3c7e
54m4n <pflum.tamas@gmail.com>
2024-03-02 09:25:05 +0100

    0

--- 06-prog-alapok-1-20240302/repcsi.py
+++ 06-prog-alapok-1-20240302/repcsi.py
@@ -1,7 +1,7 @@
 #base:
-#Egy repülőgéppel Európából Amerikába repültünk. Az út során X kilométerenként mértük a felszín tengerszint feletti magasságát. Feltételezésünk szerint ez a magasság mindenhol ≥0. 0 magasságot ott mértünk, ahol tenger van, >0-t pedig ott, ahol szárazföld
+#Egy repülőgéppel Európából Amerikába repültünk. Az út során X kilométerenként mértük a felszín tengerszint feletti magasságát. Feltételezésünk szerint ez a magasság mindenhol >=0. 0 magasságot ott mértünk, ahol tenger van, >0-t pedig ott, ahol szárazföld
 
-#eloszor kigeneraljuk a szinteket (legyen 0-10-ig), a hossz legyen 50, es beleirjuk egy fileba
+#eloszor kigeneraljuk a szinteket (legyen 0-10-ig), a meres legyen 50, es beleirjuk egy fileba
 
 from random import *
 import os
@@ -14,3 +14,20 @@ for i in range(50):
     f.write(str(randrange(0,10)))
 f.close()
 '''
+
+#beolvassuk a file tartalmat egy stringbe
+f=open(f'{path}{os.sep}travel.txt','r')
+for i in f:
+    travel=i
+
+#atalakitjuk tombbe es int-e is, mert matematikai muveleteket stringgel nemtudunk (de igen amugy) vegezni:
+measure=[]
+for i in range(len(travel)):
+    measure.append(int((travel[i:i+1:1])))
+f.close()
+
+#01.
+#Készíts programot, amely meghatározza a legszélesebb sziget bal-, illetve jobboldali partját!
+#komment: sziget akkor van, ha a ket oldala 0. ezek kozul kell a maxot kivalasztani
+
+
